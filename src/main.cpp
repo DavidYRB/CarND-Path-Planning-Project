3ee3dc721f1c24de6e3d9b9578b30cdf65082a2b
DavidYRB <david.yanrb@gmail.com>
2020-08-10 20:04:08 -0400

    Add trajectory generation and use spline to smooth the trajectory

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -97,8 +98,92 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          double ref_v = 49.5;
+          double dist_c = 0.4;
+          int lane_num = 0;
+          int traj_pts_num = 50;
 
+          vector<double> ptsx;
+          vector<double> ptsy;
 
+          int prev_size = previous_path_x.size();
+          double anchor_x = car_x;
+          double anchor_y = car_y;
+          double anchor_theta = deg2rad(car_yaw);
+
+          if(prev_size < 2){
+            double prev_x = car_x - cos(car_yaw);
+            double prev_y = car_y - sin(car_yaw);
+            ptsx.push_back(prev_x);
+            ptsx.push_back(car_x);
+            ptsy.push_back(prev_y);
+            ptsy.push_back(car_y);
+          }
+          else{
+            anchor_x = previous_path_x[prev_size-1];
+            anchor_y = previous_path_y[prev_size-1];
+            
+            double prev_x = previous_path_x[prev_size-2];
+            double prev_y = previous_path_y[prev_size-2];
+
+            ptsx.push_back(prev_x);
+            ptsx.push_back(anchor_x);
+            ptsy.push_back(prev_y);
+            ptsy.push_back(anchor_y);
+            
+            anchor_theta = atan2(anchor_y - prev_y, anchor_x - prev_x);
+          }
+
+          
+          vector<int> dist_interval = {30, 60, 90};
+          vector<double> wp1 = getXY(car_s + dist_interval[0], (2+4*lane_num), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> wp2 = getXY(car_s + dist_interval[1], (2+4*lane_num), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> wp3 = getXY(car_s + dist_interval[2], (2+4*lane_num), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          ptsx.push_back(wp1[0]);
+          ptsx.push_back(wp2[0]);
+          ptsx.push_back(wp3[0]);
+          ptsy.push_back(wp1[1]);
+          ptsy.push_back(wp2[1]);
+          ptsy.push_back(wp3[1]);
+
+          for(int i = 0; i < ptsx.size(); ++i){
+            double shift_x = ptsx[i] - anchor_x;
+            double shift_y = ptsy[i] - anchor_y;
+            ptsx[i] = shift_x*cos(0 - anchor_theta) - shift_y*sin(0 - anchor_theta);
+            ptsy[i] = shift_x*sin(0 - anchor_theta) + shift_y*cos(0 - anchor_theta);
+          }
+
+          tk::spline s;
+
+          s.set_points(ptsx, ptsy);
+
+          for(int i = 0; i < prev_size; ++i){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+          
+          double target_x = 30;
+          double target_y = s(target_x);
+          double target_dist = sqrt((target_x*target_x) + (target_y*target_y));
+          double increment_dist = target_x / (target_dist/(0.02 * ref_v / 2.24));
+          double temp_x = 0;
+          double temp_y = 0;
+          double global_x;
+          double global_y;
+          for(int i = 1; i <= traj_pts_num - prev_size; ++i){
+            temp_x += increment_dist;
+            temp_y = s(temp_x);
+
+            global_x = temp_x*cos(anchor_theta) - temp_y*sin(anchor_theta);
+            global_y = temp_x*sin(anchor_theta) + temp_y*cos(anchor_theta);
+            global_x += anchor_x;
+            global_y += anchor_y;
+
+            next_x_vals.push_back(global_x);
+            next_y_vals.push_back(global_y);
+          }
+          //std::cout << "trajectory size: " << next_x_vals.size() << std::endl;
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
